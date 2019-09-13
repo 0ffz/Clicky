bf40352fe06c7043688ddb6fcfb522fbd508bc13
0ffz <dan.voznyy@gmail.com>
2019-09-13 19:10:07 -0400

    Added view results button when creator on vote page.

--- Clicky/templatetags/clickytags.py
+++ Clicky/templatetags/clickytags.py
@@ -1,8 +1,15 @@
 from django import template
 
+from Clicky.views import get_room_admin
+
 register = template.Library()
 
 
 @register.filter
 def sort_by(queryset, order):
     return queryset.order_by(order)
+
+
+@register.filter
+def is_room_admin(session, room_id):
+    return get_room_admin(room_id) in session
