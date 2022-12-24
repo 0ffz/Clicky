0f344496da04e73183ba2399a1044563f2884247
0ffz <dan.voznyy@gmail.com>
2022-12-24 18:13:22 -0500

    Update syntax for creating database

--- main/settings.py
+++ main/settings.py
@@ -82,10 +82,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
@@ -118,10 +119,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 
 def get_captcha_key():
     try:
